c1aaaf1c74e2891e03390c8e6dcbf29b95cf2820
Mukhtar Turarbek <mukhtar.nayman@mail.ru>
2026-03-10 21:15:45 +0500

    more stuff and CI added

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -26,6 +26,10 @@ from sklearn.ensemble import (
 
 import mlflow
 
+import dagshub
+
+dagshub.init(repo_owner='SmilerKZ', repo_name='NetworkSecurity', mlflow=True)
+
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
